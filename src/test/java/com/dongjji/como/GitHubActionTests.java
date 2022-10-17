ebb01bf5fae68bf2f556c8426aed85fe089f647c
DongJoon Cha <y2kdj9723@naver.com>
2022-10-18 04:13:06 +0900

    Test PR

---
+++ src/test/java/com/dongjji/como/GitHubActionTests.java
@@ -0,0 +1,14 @@
+package com.dongjji.como;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class GitHubActionTests {
+    @Test
+    void githubActionTest() {
+        assertEquals(1, 1);
+    }
+}
